a31a899278ca0ae5f64795a6ce07e1639dcbb76c
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2023-10-30 19:00:56 +0100

    initial commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,6 +6,7 @@ import sys
 
 """check if script is being run"""
 if __name__ == "__main__":
+    """check if there is exactly one command-line argument provided"""
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
         print("Usage: python 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
