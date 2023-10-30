8daf1bdf125e823f3ad0bc96d5b8c0e30ec9f16d
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2023-10-30 19:02:42 +0100

    initial commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,19 +5,16 @@
 import requests
 import sys
 
-"""check if script is being run"""
 if __name__ == "__main__":
-    """check if there is exactly one command-line argument provided"""
+    
     if len(sys.argv) != 2 or not sys.argv[1].isdigit():
         print("Usage: python 0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
 
-    """ Define the base URL for the API """
     base_url = "https://jsonplaceholder.typicode.com"
 
-    """Fetch user data"""
     user_response = requests.get(f"{base_url}/users/{employee_id}")
     todos_response = requests.get(f"{base_url}/todos?userId={employee_id}")
 
@@ -30,15 +27,12 @@ if __name__ == "__main__":
 
     employee_name = user_data["name"]
 
-    """Calculate the number of completed tasks"""
     completed_tasks = [task for task in todos_data if task["completed"]]
     number_of_done_tasks = len(completed_tasks)
     total_number_of_tasks = len(todos_data)
 
-    """Print employee information"""
     print(f"Employee {employee_name} is done with tasks
           ({number_of_done_tasks}/{total_number_of_tasks}):")
 
-"""Print completed task titles"""
 for task in completed_tasks:
       print(f"\t{task['title']}")
