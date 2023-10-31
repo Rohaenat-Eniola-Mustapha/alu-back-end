601a89ab6c101d156528ffa3f9a54994320a404c
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2023-10-31 13:14:22 +0100

    initial commit

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,42 +1,34 @@
 #!/usr/bin/python3
 """
-    exporting data in the JSON format
+Exporting data in the JSON format.
 """
+
 import json
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    """
-        requesting user info by employee ID
-    """
+    # Requesting user info by employee ID
     request_employee = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
-    """
-        converting json to dictionary
-    """
+
+    # Converting JSON to a dictionary
     user = json.loads(request_employee.text)
-    """
-        extracting username
-    """
+
+    # Extracting the username
     username = user.get("username")
 
-    """
-        requesting user's TODO list
-    """
+    # Requesting user's TODO list
     request_todos = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
-    """
-        dictionary to store task status in boolean format
-    """
+
+    # Dictionary to store task status in boolean format
     tasks = {}
-    """
-        converting json to list of dictionaries
-    """
+
+    # Converting JSON to a list of dictionaries
     user_todos = json.loads(request_todos.text)
-    """
-        looping through dictionary & get completed tasks
-    """
+
+    # Looping through dictionaries and getting completed tasks
     for dictionary in user_todos:
         tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
@@ -49,8 +41,8 @@ if __name__ == "__main__":
         })
 
     json_to_dump = {argv[1]: task_list}
-    """
-        exporting to JSON
-    """
+
+    # Exporting to JSON
     with open('{}.json'.format(argv[1]), mode='w') as file:
         json.dump(json_to_dump, file)
+        
