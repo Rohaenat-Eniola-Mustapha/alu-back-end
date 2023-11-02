e6025b015f443aa6b9cce136a57a9ca5a3433d47
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2023-11-02 13:55:37 +0000

    done

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """ Import libraries """
-=======
-"""
-This script retrieves and displays a user's TODO list 
-progress using a REST API.
-Usage: python3 gather_data_from_api.py <employee_id>
-"""
->>>>>>> ffae77ac608018668f281303944f7e87b3677a11
 
 import requests
 import sys
 
-<<<<<<< HEAD
 """Gathering data from an API """
 
 if __name__ == "__main__":
@@ -34,46 +25,3 @@ if __name__ == "__main__":
           task_com, total_task_done))
 
     [print("\t {}".format(task.get("title"))) for task in total_tasks]
-=======
-
-def get_todo_list_progress(employee_id):
-    """
-    Fetches and displays the TODO list progress for a given employee ID.
-
-    Args:
-        employee_id (str): The ID of the employee.
-
-    Returns:
-        None
-    """
-    # Construct URLs for user and TODO data
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
-    try:
-        # Fetch user and TODO data from the API
-        user_info = requests.get(user_url).json()
-        todo_info = requests.get(todo_url).json()
-
-        # Extract employee name and completed tasks
-        empl_name = user_info.get("name")
-        completed_tasks = [task for task in todo_info if task["completed"]]
-        compl_count = len(completed_tasks)
-        ttl_count = len(todo_info)
-
-        # Display the progress information
-        print(f"Employee {empl_name} is done with tasks({compl_count}/{ttl_count}):")
-
-        for task in completed_tasks:
-            print(f"\t{task['title']}")
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_api.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    get_todo_list_progress(employee_id)
->>>>>>> ffae77ac608018668f281303944f7e87b3677a11
--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,30 +1,15 @@
 #!/usr/bin/python3
 """
-<<<<<<< HEAD
     python script that exports data in the JSON format
 """
 import json
 import requests
 
-=======
-    exporting data in the JSON format
-"""
-
-
-import json
-import requests
-
-
->>>>>>> ffae77ac608018668f281303944f7e87b3677a11
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
     """
-<<<<<<< HEAD
         export to JSON
-=======
-        exporting to JSON
->>>>>>> ffae77ac608018668f281303944f7e87b3677a11
     """
 
     with open("todo_all_employees.json", "w") as jsonfile:
@@ -35,8 +20,4 @@ if __name__ == "__main__":
                 "username": u.get("username")
             } for t in requests.get(url + "todos",
                                     params={"userId": u.get("id")}).json()]
-<<<<<<< HEAD
             for u in users}, jsonfile)
-=======
-            for u in users}, jsonfile)
->>>>>>> ffae77ac608018668f281303944f7e87b3677a11
