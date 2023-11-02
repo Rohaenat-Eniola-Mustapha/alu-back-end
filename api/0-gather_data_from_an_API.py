3b1c9e99a202022ed667323296b1d745e47d93c3
Rohaenat-Eniola-Mustapha <r.mustapha@alustudent.com>
2023-11-02 13:49:48 +0100

    changes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -32,15 +32,13 @@ def get_todo_list_progress(employee_id):
         todo_info = requests.get(todo_url).json()
 
         # Extract employee name and completed tasks
-        employee_name = user_info.get("name")
+        empl_name = user_info.get("name")
         completed_tasks = [task for task in todo_info if task["completed"]]
         compl_count = len(completed_tasks)
         ttl_count = len(todo_info)
 
         # Display the progress information
-        print(
-            f"Employee {employee_name} is done with tasks({compl_count}/{ttl_count}):"
-            )
+        print(f"Employee {empl_name} is done with tasks({compl_count}/{ttl_count}):")
         for task in completed_tasks:
             print(f"\t{task['title']}")
     except requests.exceptions.RequestException as e:
